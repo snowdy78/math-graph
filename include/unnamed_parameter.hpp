ac6dff85cb2316f655c6b10af87891fbf8369fb1
snowdy78 <themrleoner12@mail.ru>
2026-03-17 00:09:58 +0700

    refactor parameter class and add unnamed_parameter class

---
+++ include/unnamed_parameter.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "mgraphfwd.hpp"
+#include "variable.hpp"
+#include "number.hpp"
+#include <variant>
+
+namespace mg
+{
+	class unnamed_parameter
+	{
+	protected:
+		using num_or_var_t = std::variant<number, variable>;
+
+	private:
+		num_or_var_t m_value;
+		num_or_var_t parse_parameter(const string_type &value)
+		{
+			try
+			{
+				return variable(value.c_str());
+			}
+			catch (std::runtime_error &)
+			{
+				try
+				{
+					return number(value.c_str());
+				}
+				catch (std::runtime_error &err)
+				{
+					throw std::runtime_error("can't create parameter by '" + value + "'");
+				}
+			}
+		}
+
+	public:
+		unnamed_parameter(const string_type &value)
+			: m_value(parse_parameter(value))
+		{}
+		explicit unnamed_parameter(const num_or_var_t &value)
+			: m_value(value)
+		{}
+		bool is_variable() const
+		{
+			return std::holds_alternative<variable>(m_value);
+		}
+		bool is_number() const
+		{
+			return std::holds_alternative<number>(m_value);
+		}
+		const number &as_number() const
+		{
+			return std::get<number>(m_value);
+		}
+		const variable &as_variable() const
+		{
+			return std::get<variable>(m_value);
+		}
+	};
+} // namespace mg
