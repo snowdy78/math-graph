f60ddf3220bf7ab0d7d3722a0af0eeb4984a1b48
snowdy78 <themrleoner12@mail.ru>
2026-03-18 08:35:56 +0700

    add precision to pure number string

--- include/pure_output.hpp
+++ include/pure_output.hpp
@@ -4,18 +4,25 @@
 
 namespace mg
 {
-	inline string_type to_pure_string(double num)
+	inline string_type to_pure_string(double num, size_t precision = 0)
 	{
 		auto str = std::to_string(num);
+		if (str.find('e') != std::string::npos)
+			return str;
+		size_t pos = str.find('.');
+		if (pos == std::string::npos)
+			return str;
+		size_t dot_distance = str.size() - pos - 1;
 		while (str.back() == '0')
 		{
 			str.pop_back();
-			if (str.back() == '.')
+			if (--dot_distance == precision)
 			{
-				str.pop_back();
 				break;
 			}
 		}
+		if (str.back() == '.')
+			str.pop_back();
 		return str;
 	}
 } // namespace mg
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "operator_action.hpp"
-#include "parameter.hpp"
 #include "unresolved_expression.hpp"
 
 using namespace std::string_literals;
@@ -9,7 +8,6 @@ int main()
 {
 	mg::number n{ "3.14" };
 	mg::variable v{ "x" };
-	mg::parameter p("p", { n });
 	mg::operator_action pa("2 + x");
 	std::cout << "'" << std::get<mg::unresolved_expression>(pa.compute()).str(" ") << "'\n";
 	return 0;
