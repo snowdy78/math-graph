3fbc17cf434fb430516f24a4afdbfba358ea81f2
snowdy78 <themrleoner12@mail.ru>
2026-03-19 19:31:35 +0700

    rewrite operator action

--- include/operator_action.hpp
+++ include/operator_action.hpp
@@ -1,39 +1,22 @@
 #pragma once
 
-#include "mgraphfwd.hpp"
 #include "operation.hpp"
-#include "unresolved_expression.hpp"
-#include <array>
+#include "independent_variable.hpp"
 #include <optional>
+#include <variant>
 
 namespace mg
 {
 	class operator_action
 	{
 	public:
-		using result_type = std::variant<unresolved_expression, number>;
+		using forward_type = std::variant<number, independent_variable, const operator_action *>;
 
 	private:
-		constexpr static const char *s_operator_action_pattern = R"((.+)(\s+)(.)(\s+)(.+))";
-		constexpr static const char *s_function_action_pattern = "";
-		void parse(const string_type &str)
-		{
-			std::regex op_rgx(s_operator_action_pattern);
-			std::smatch match;
-			if (!std::regex_search(str, match, op_rgx))
-			{
-				throw std::runtime_error("unable to create expression by '" + str + "'");
-			}
-			std::array<string_type, 3> words = { match[1], match[3], match[5] };
-			m_left							 = words[0];
-			m_op							 = &operation::get_by_name(words[1][0]);
-			m_right							 = words[2];
-		}
-		std::optional<unnamed_parameter> m_left, m_right;
-		const operation *m_op = nullptr;
+		void parse(const string_type &str);
 
 	public:
-		operator_action(const unnamed_parameter &opleft, const operation &op, const unnamed_parameter &opright)
+		operator_action(const forward_type &opleft, const operation &op, const forward_type &opright)
 			: m_left(opleft),
 			  m_right(opright),
 			  m_op(&op)
@@ -42,11 +25,15 @@ namespace mg
 		{
 			parse(action_str);
 		}
-		const unnamed_parameter &left() const
+		const forward_type &left() const
 		{
 			return *m_left;
 		}
-		const unnamed_parameter &right() const
+		const set_dependencies &deps() const
+		{
+			return m_deps;
+		}
+		const forward_type &right() const
 		{
 			return *m_right;
 		}
@@ -54,15 +41,12 @@ namespace mg
 		{
 			return *m_op;
 		}
-		result_type compute() const
-		{
-			if (m_left->is_variable() || m_right->is_variable())
-			{
-				auto &guarantee_var = m_left->is_variable() ? *m_left : *m_right;
-				auto &probably_var	= m_left->is_variable() ? *m_right : *m_left;
-				return unresolved_expression(guarantee_var.as_variable()).push(*m_op, probably_var);
-			}
-			return operation::get_compute_map().at(&*m_op)(m_left->as_number(), m_right->as_number());
-		}
+
+	private:
+		constexpr static const char *s_operator_action_pattern = R"((.+)(\s+)(.)(\s+)(.+))";
+
+		set_dependencies m_deps;
+		std::optional<forward_type> m_left, m_right;
+		const operation *m_op = nullptr;
 	};
 } // namespace mg
---
+++ src/operator_action.cpp
@@ -0,0 +1,27 @@
+#include "operator_action.hpp"
+#include "tools.hpp"
+
+namespace mg
+{
+	void operator_action::parse(const string_type &str)
+	{
+		std::regex op_rgx(s_operator_action_pattern);
+		std::smatch match;
+		if (!std::regex_search(str, match, op_rgx))
+		{
+			throw std::runtime_error("unable to create expression by '" + str + "'");
+		}
+		string_type words[] = { match[1], match[3], match[5] };
+		m_left				= create_parameter_data(words[0]);
+		if (std::holds_alternative<independent_variable>(*m_left))
+		{
+			m_deps.insert(std::get<independent_variable>(*m_left));
+		}
+		m_op	= &operation::get_by_name(words[1][0]);
+		m_right = create_parameter_data(words[2]);
+		if (std::holds_alternative<independent_variable>(*m_right))
+		{
+			m_deps.insert(std::get<independent_variable>(*m_right));
+		}
+	}
+} // namespace mg
