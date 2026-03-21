697a6fbf1fb178414316af9c98494a01d1a7a96a
snowdy78 <themrleoner12@mail.ru>
2026-03-21 12:12:52 +0700

    operator_action tests

--- test/operator_action.hpp
+++ test/operator_action.hpp
@@ -1,1 +1,213 @@
+#include "catch2/catch_test_macros.hpp"
+#include "math_graph/operator_action.hpp"
 
+TEST_CASE("operator_action construct", "[test]")
+{
+	SECTION("construct by correct string")
+	{
+		SECTION("two numbers")
+		{
+			mg::operator_action op{ "1 + 2" };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 0);
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.left()) == 1.0);
+			REQUIRE(std::get<mg::operator_action::number_type>(op.right()) == 2.0);
+		}
+		SECTION("var and number")
+		{
+			mg::operator_action op{ "x + 2" };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 1);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.right()) == 2.0);
+		}
+		SECTION("var and var")
+		{
+			mg::operator_action op{ "x + x1" };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 2);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(op.deps().contains({ "x1" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.right()));
+		}
+		SECTION("by negative number")
+		{
+			mg::operator_action op{ "-1 + 2" };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 0);
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.left()) == -1.0);
+			REQUIRE(std::get<mg::operator_action::number_type>(op.right()) == 2.0);
+		}
+	}
+	SECTION("construct by incorrect string")
+	{
+		SECTION("empty string")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "" });
+		}
+		SECTION("wrong string")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "a@43$FSA_fas" });
+		}
+		SECTION("too many operations")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "1 + 2 + 3" });
+		}
+		SECTION("wrong operands")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "1 + 2a" });
+			REQUIRE_THROWS(mg::operator_action{ "$fa + 2" });
+		}
+		SECTION("less operands")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "1 + " });
+			REQUIRE_THROWS(mg::operator_action{ "+" });
+			REQUIRE_THROWS(mg::operator_action{ "- 1" });
+		}
+		SECTION("wrong operation")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "1 _ 2" });
+			REQUIRE_THROWS(mg::operator_action{ "1 a 2" });
+		}
+		SECTION("negative var")
+		{
+			REQUIRE_THROWS(mg::operator_action{ "-x + 1" });
+			REQUIRE_NOTHROW(mg::operator_action{ "+1 - x" });
+		}
+	}
+	SECTION("construct by values")
+	{
+		SECTION("two numbers")
+		{
+			mg::operator_action op{ 1.0, mg::unique_operations::add, 2.0 };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 0);
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.left()) == 1.0);
+			REQUIRE(std::get<mg::operator_action::number_type>(op.right()) == 2.0);
+		}
+
+		SECTION("number and var")
+		{
+			mg::operator_action::variable_type x{ "x" };
+			mg::operator_action op{ 1.0, mg::unique_operations::add, x };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 1);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.left()) == 1.0);
+		}
+		SECTION("var and number")
+		{
+			mg::operator_action::variable_type x{ "x" };
+			mg::operator_action op{ x, mg::unique_operations::add, 1.0 };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 1);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.right()) == 1.0);
+		}
+		SECTION("var and var")
+		{
+			mg::operator_action::variable_type x{ "x" }, x1{ "x1" };
+			mg::operator_action op{ x, mg::unique_operations::add, x1 };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 2);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(op.deps().contains({ "x1" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.right()));
+		}
+		SECTION("except wrong provided operator action")
+		{
+			REQUIRE_THROWS(mg::operator_action{ 1.0, mg::unique_operations::add, nullptr });
+		}
+		SECTION("number and other operation")
+		{
+			mg::operator_action p{ "x + 2" };
+			mg::operator_action op{ 1.0, mg::unique_operations::add, &p };
+			REQUIRE(op.op() == mg::unique_operations::add);
+			REQUIRE(op.deps().size() == 1);
+			REQUIRE(op.deps().contains({ "x" }));
+			REQUIRE(std::holds_alternative<mg::operator_action::number_type>(op.left()));
+			REQUIRE(std::holds_alternative<mg::operator_action::pointer_type>(op.right()));
+			REQUIRE(std::get<mg::operator_action::number_type>(op.left()) == 1.0);
+		}
+		SECTION("var and other operation")
+		{
+			auto test = [](mg::string_type opact_string, mg::string_type var_name) {
+				mg::operator_action p{ opact_string };
+				mg::independent_variable x{ var_name };
+				mg::operator_action op{ x, mg::unique_operations::add, &p };
+				REQUIRE(op.op() == mg::unique_operations::add);
+				REQUIRE(std::holds_alternative<mg::operator_action::variable_type>(op.left()));
+				REQUIRE(std::holds_alternative<mg::operator_action::pointer_type>(op.right()));
+				return op;
+			};
+			SECTION("with same vars")
+			{
+				auto op = test("x + 2", "x");
+				REQUIRE(op.deps().size() == 1);
+				REQUIRE(op.deps().contains({ "x" }));
+			}
+			SECTION("with different vars")
+			{
+				auto op = test("x + 2", "y");
+				REQUIRE(op.deps().size() == 2);
+				REQUIRE(op.deps().contains({ "x" }));
+				REQUIRE(op.deps().contains({ "y" }));
+			}
+			SECTION("with many different vars")
+			{
+				auto op = test("x + z", "y");
+				REQUIRE(op.deps().size() == 3);
+				REQUIRE(op.deps().contains({ "x" }));
+				REQUIRE(op.deps().contains({ "y" }));
+				REQUIRE(op.deps().contains({ "z" }));
+			}
+		}
+		SECTION("operation and other operation")
+		{
+			auto test = [](mg::string_type opact_string, mg::string_type other_opact_string) {
+				mg::operator_action p{ opact_string }, x{ other_opact_string };
+				mg::operator_action op{ &x, mg::unique_operations::add, &p };
+				REQUIRE(op.op() == mg::unique_operations::add);
+				REQUIRE(std::holds_alternative<mg::operator_action::pointer_type>(op.left()));
+				REQUIRE(std::holds_alternative<mg::operator_action::pointer_type>(op.right()));
+				return op;
+			};
+			SECTION("with same vars")
+			{
+				auto op = test("x + x", "x + 3");
+				REQUIRE(op.deps().size() == 1);
+				REQUIRE(op.deps().contains({ "x" }));
+			}
+			SECTION("with different vars")
+			{
+				auto op = test("x + 2", "y + 3");
+				REQUIRE(op.deps().size() == 2);
+				REQUIRE(op.deps().contains({ "x" }));
+				REQUIRE(op.deps().contains({ "y" }));
+			}
+			SECTION("with many different vars")
+			{
+				auto op = test("x + z", "y + w");
+				REQUIRE(op.deps().size() == 4);
+				REQUIRE(op.deps().contains({ "x" }));
+				REQUIRE(op.deps().contains({ "y" }));
+				REQUIRE(op.deps().contains({ "z" }));
+				REQUIRE(op.deps().contains({ "w" }));
+			}
+		}
+	}
+}
