f7b50467907c69515bececccbfce37795ec4a35d
snowdy78 <themrleoner12@mail.ru>
2026-03-21 09:10:09 +0700

    independent_variable tests

---
+++ test/independent_variable.cpp
@@ -0,0 +1,47 @@
+#include <catch2/catch_test_macros.hpp>
+#include "independent_variable.hpp"
+
+TEST_CASE("independent_variable construct", "[.independent_variable]")
+{
+	SECTION("construct by name")
+	{
+		mg::independent_variable x{ "x" };
+		REQUIRE(x.name() == "x");
+
+		mg::independent_variable x1{ "x12" };
+		REQUIRE(x1.name() == "x12");
+	}
+	SECTION("parse error")
+	{
+		try
+		{
+
+			mg::independent_variable x{ "1" };
+		}
+		catch (std::exception &err)
+		{
+			REQUIRE(true);
+		}
+		try
+		{
+			mg::independent_variable x{ "1a" };
+		}
+		catch (std::exception &err)
+		{
+			REQUIRE(true);
+		}
+		try
+		{
+			mg::independent_variable x{ "_x" };
+		}
+		catch (std::exception &err)
+		{
+			REQUIRE(true);
+		}
+	}
+}
+TEST_CASE("independent_variable ", "[.independent_variable]")
+{
+	mg::independent_variable x{ "x" };
+	REQUIRE(x.name() == "x");
+}
