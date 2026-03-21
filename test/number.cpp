4cc77456a514b5f67b779ee710e577f7604f07a3
snowdy78 <themrleoner12@mail.ru>
2026-03-21 08:47:35 +0700

    number tests

--- test/number.cpp
+++ test/number.cpp
@@ -1,1 +1,48 @@
 #include <catch2/catch_test_macros.hpp>
+
+#include "number.hpp"
+
+TEST_CASE("construct number", "[.number]")
+{
+	SECTION("construct by number")
+	{
+		mg::number n{ 2.0 };
+		REQUIRE(n == 2.0);
+	}
+	SECTION("construct by string")
+	{
+		mg::number n{ "2.0" };
+		REQUIRE(n == 2.0);
+		n = { "2" };
+		REQUIRE(n == 2.0);
+		n = { "2." };
+		REQUIRE(n == 2.0);
+	}
+}
+TEST_CASE("operations with number", "[.number]")
+{
+	SECTION("add")
+	{
+		mg::number n1{ "2.0" };
+		mg::number n2{ "2.0" };
+		REQUIRE(n1 + n2 == 4.0);
+	}
+	SECTION("mult")
+	{
+		mg::number n1{ "2.0" };
+		mg::number n2{ "2.0" };
+		REQUIRE(n1 * n2 == 4.0);
+	}
+	SECTION("divide")
+	{
+		mg::number n1{ "2.0" };
+		mg::number n2{ "2.0" };
+		REQUIRE(n1 / n2 == 1.0);
+	}
+	SECTION("substract")
+	{
+		mg::number n1{ "2.0" };
+		mg::number n2{ "2.0" };
+		REQUIRE(n1 - n2 == 0.0);
+	}
+}
